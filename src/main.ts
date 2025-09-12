1a5d5409a97a114e52d983ae23ba65326ed9a3de
sedovev <sedovev@rgn.gov.ru>
2025-09-12 15:07:29 +0700

    adds pinia

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import "./style.css";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
 app.use(PrimeVue, {
   ripple: true,
   theme: {
