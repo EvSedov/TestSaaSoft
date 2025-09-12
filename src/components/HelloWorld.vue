a76639f28cd43491385e1010abab76b2a3422acb
sedovev <sedovev@rgn.gov.ru>
2025-09-12 15:05:35 +0700

    adds primevue ui and primevue-tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -32,6 +32,26 @@ const count = ref(0)
     >.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+  <div class="flex flex-col gap-12">
+    <div class="flex gap-6 flex-wrap">
+      <div
+        class="rounded-border p-4 border border-transparent flex items-center justify-center bg-primary hover:bg-primary-emphasis text-primary-contrast font-medium flex-auto transition-colors"
+      >
+        primary
+      </div>
+      <div
+        class="rounded-border p-4 border border-transparent flex items-center justify-center bg-highlight hover:bg-highlight-emphasis font-medium flex-auto transition-colors"
+      >
+        highlight
+      </div>
+      <div
+        class="rounded-border p-4 border border-surface flex items-center justify-center text-muted-color hover:text-color hover:bg-emphasis font-medium flex-auto transition-colors"
+      >
+        box
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
